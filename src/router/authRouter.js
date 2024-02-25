95900db56b5365763b953b1da6538d306dc70f9a
Arjesh Khadka <arjesh.khadka2@gmail.com>
2024-02-26 10:23:24 +1100

    fix: typo fix

--- src/router/authRouter.js
+++ src/router/authRouter.js
@@ -8,7 +8,7 @@ router.route('/login').post(validateLoginAuthValues ,loginUser)
 router.route('/user').put(validateUpdateAuthValues, updateUserDetails)
 router.route('/user/changePassword').put(validateChangePasswordValues, changePassword)
 router.route('/sendOTP').post(validateResetPassword, sendOTP)
-router.route('/vefiryCode').post(verfiyCode)
+router.route('/verifyCode').post(verfiyCode)
 router.route('/resetPassword').post(resetPassword)
 
 export default router
